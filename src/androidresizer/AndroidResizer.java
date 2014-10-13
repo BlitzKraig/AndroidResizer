a7f1033908e74dcc8ad60811cdb5623beaec0069
Craig Currie <craig.currie@planysdigital.com>
2014-10-13 23:02:56 +0100

    Initial Commit

---
+++ src/androidresizer/AndroidResizer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package androidresizer;
+
+/**
+ *
+ * @author planys
+ */
+public class AndroidResizer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        System.out.println("TEST");
+        new NewJFrame().setVisible(true);
+    }
+    
+}
